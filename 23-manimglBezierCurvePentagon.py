d988ad3a064cda5edaa6faed1cb1ee94095d51cf
LinZeZhi <LinZeZhi1@gmail.com>
2025-04-25 16:24:35 +0800

    test：使用 Manimgl 的 Stroke 逻辑渲染五条 BezierCurve，形成封闭五边形

--- 23-manimglBezierCurvePentagon.py
+++ 23-manimglBezierCurvePentagon.py
@@ -8,6 +8,8 @@ from OpenGL.GL import *
 import ctypes
 import math
 
+# test：使用 Manimgl 的 Stroke 逻辑渲染五条 BezierCurve，形成封闭五边形
+
 # 顶点着色器 (Vertex Shader) - 与之前相同
 VERTEX_SHADER_SRC = """
 #version 330 core
